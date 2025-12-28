f596bb95aa12f65dad8444b771fd1f608972f2b2
James Swent <jake@osprey21.com>
2025-12-28 11:33:25 -0500

    Use clang-format-15 formatter

--- deps/src/constants.cpp
+++ deps/src/constants.cpp
@@ -8,72 +8,97 @@
 
 namespace libsemigroups_julia {
 
-void define_constants(jl::Module &mod) {
+void define_constants(jl::Module & m)
+{
   // UNDEFINED values for different integer types
   // UNDEFINED represents max value of the type
-  mod.method("UNDEFINED_UInt8",
-             []() -> uint8_t { return libsemigroups::UNDEFINED; });
-  mod.method("UNDEFINED_UInt16",
-             []() -> uint16_t { return libsemigroups::UNDEFINED; });
-  mod.method("UNDEFINED_UInt32",
-             []() -> uint32_t { return libsemigroups::UNDEFINED; });
-  mod.method("UNDEFINED_UInt64",
-             []() -> uint64_t { return libsemigroups::UNDEFINED; });
-  mod.method("UNDEFINED_Int64",
-             []() -> int64_t { return libsemigroups::UNDEFINED; });
+  m.method("UNDEFINED_UInt8", []() -> uint8_t {
+    return libsemigroups::UNDEFINED;
+  });
+  m.method("UNDEFINED_UInt16", []() -> uint16_t {
+    return libsemigroups::UNDEFINED;
+  });
+  m.method("UNDEFINED_UInt32", []() -> uint32_t {
+    return libsemigroups::UNDEFINED;
+  });
+  m.method("UNDEFINED_UInt64", []() -> uint64_t {
+    return libsemigroups::UNDEFINED;
+  });
+  m.method("UNDEFINED_Int64", []() -> int64_t {
+    return libsemigroups::UNDEFINED;
+  });
 
   // POSITIVE_INFINITY values for different integer types
   // POSITIVE_INFINITY represents max-1 value of the type
-  mod.method("POSITIVE_INFINITY_UInt8",
-             []() -> uint8_t { return libsemigroups::POSITIVE_INFINITY; });
-  mod.method("POSITIVE_INFINITY_UInt16",
-             []() -> uint16_t { return libsemigroups::POSITIVE_INFINITY; });
-  mod.method("POSITIVE_INFINITY_UInt32",
-             []() -> uint32_t { return libsemigroups::POSITIVE_INFINITY; });
-  mod.method("POSITIVE_INFINITY_UInt64",
-             []() -> uint64_t { return libsemigroups::POSITIVE_INFINITY; });
-  mod.method("POSITIVE_INFINITY_Int64",
-             []() -> int64_t { return libsemigroups::POSITIVE_INFINITY; });
+  m.method("POSITIVE_INFINITY_UInt8", []() -> uint8_t {
+    return libsemigroups::POSITIVE_INFINITY;
+  });
+  m.method("POSITIVE_INFINITY_UInt16", []() -> uint16_t {
+    return libsemigroups::POSITIVE_INFINITY;
+  });
+  m.method("POSITIVE_INFINITY_UInt32", []() -> uint32_t {
+    return libsemigroups::POSITIVE_INFINITY;
+  });
+  m.method("POSITIVE_INFINITY_UInt64", []() -> uint64_t {
+    return libsemigroups::POSITIVE_INFINITY;
+  });
+  m.method("POSITIVE_INFINITY_Int64", []() -> int64_t {
+    return libsemigroups::POSITIVE_INFINITY;
+  });
 
   // LIMIT_MAX values for different integer types
   // LIMIT_MAX represents max-2 value of the type
-  mod.method("LIMIT_MAX_UInt8",
-             []() -> uint8_t { return libsemigroups::LIMIT_MAX; });
-  mod.method("LIMIT_MAX_UInt16",
-             []() -> uint16_t { return libsemigroups::LIMIT_MAX; });
-  mod.method("LIMIT_MAX_UInt32",
-             []() -> uint32_t { return libsemigroups::LIMIT_MAX; });
-  mod.method("LIMIT_MAX_UInt64",
-             []() -> uint64_t { return libsemigroups::LIMIT_MAX; });
-  mod.method("LIMIT_MAX_Int64",
-             []() -> int64_t { return libsemigroups::LIMIT_MAX; });
+  m.method("LIMIT_MAX_UInt8", []() -> uint8_t {
+    return libsemigroups::LIMIT_MAX;
+  });
+  m.method("LIMIT_MAX_UInt16", []() -> uint16_t {
+    return libsemigroups::LIMIT_MAX;
+  });
+  m.method("LIMIT_MAX_UInt32", []() -> uint32_t {
+    return libsemigroups::LIMIT_MAX;
+  });
+  m.method("LIMIT_MAX_UInt64", []() -> uint64_t {
+    return libsemigroups::LIMIT_MAX;
+  });
+  m.method("LIMIT_MAX_Int64", []() -> int64_t {
+    return libsemigroups::LIMIT_MAX;
+  });
 
   // NEGATIVE_INFINITY for signed types
-  mod.method("NEGATIVE_INFINITY_Int8",
-             []() -> int8_t { return libsemigroups::NEGATIVE_INFINITY; });
-  mod.method("NEGATIVE_INFINITY_Int16",
-             []() -> int16_t { return libsemigroups::NEGATIVE_INFINITY; });
-  mod.method("NEGATIVE_INFINITY_Int32",
-             []() -> int32_t { return libsemigroups::NEGATIVE_INFINITY; });
-  mod.method("NEGATIVE_INFINITY_Int64",
-             []() -> int64_t { return libsemigroups::NEGATIVE_INFINITY; });
+  m.method("NEGATIVE_INFINITY_Int8", []() -> int8_t {
+    return libsemigroups::NEGATIVE_INFINITY;
+  });
+  m.method("NEGATIVE_INFINITY_Int16", []() -> int16_t {
+    return libsemigroups::NEGATIVE_INFINITY;
+  });
+  m.method("NEGATIVE_INFINITY_Int32", []() -> int32_t {
+    return libsemigroups::NEGATIVE_INFINITY;
+  });
+  m.method("NEGATIVE_INFINITY_Int64", []() -> int64_t {
+    return libsemigroups::NEGATIVE_INFINITY;
+  });
 
   // tril enum for three-valued logic (true, false, unknown)
-  mod.add_bits<libsemigroups::tril>("tril", jl::julia_type("CppEnum"));
-  mod.set_const("tril_FALSE", libsemigroups::tril::FALSE);
-  mod.set_const("tril_TRUE", libsemigroups::tril::TRUE);
-  mod.set_const("tril_unknown", libsemigroups::tril::unknown);
+  m.add_bits<libsemigroups::tril>("tril", jl::julia_type("CppEnum"));
+  m.set_const("tril_FALSE", libsemigroups::tril::FALSE);
+  m.set_const("tril_TRUE", libsemigroups::tril::TRUE);
+  m.set_const("tril_unknown", libsemigroups::tril::unknown);
 
   // Helper to convert tril to Julia Bool or nothing
-  mod.method("tril_to_bool", [](libsemigroups::tril t) -> jl_value_t * {
-    if (t == libsemigroups::tril::TRUE) {
+  m.method("tril_to_bool", [](libsemigroups::tril t) -> jl_value_t * {
+    if (t == libsemigroups::tril::TRUE)
+    {
       return jl_box_bool(true);
-    } else if (t == libsemigroups::tril::FALSE) {
+    }
+    else if (t == libsemigroups::tril::FALSE)
+    {
       return jl_box_bool(false);
-    } else {
+    }
+    else
+    {
       return jl_nothing;
     }
   });
 }
 
-} // namespace libsemigroups_julia
+}    // namespace libsemigroups_julia
--- deps/src/errors.cpp
+++ deps/src/errors.cpp
@@ -11,37 +11,51 @@ namespace libsemigroups_julia {
 static std::vector<std::string> error_log;
 
 // Clear the error log
-void clear_error_log() { error_log.clear(); }
+void clear_error_log()
+{
+  error_log.clear();
+}
 
 // Add an error message to the log
-void log_error(const std::string &msg) { error_log.push_back(msg); }
+void log_error(const std::string & msg)
+{
+  error_log.push_back(msg);
+}
 
 // Check if there are any logged errors
-bool have_error() { return !error_log.empty(); }
+bool have_error()
+{
+  return !error_log.empty();
+}
 
 // Get all error messages and clear the log
-std::string get_and_clear_errors() {
+std::string get_and_clear_errors()
+{
   std::stringstream ss;
-  for (const auto &msg : error_log) {
+  for (const auto & msg : error_log)
+  {
     ss << msg << std::endl;
   }
   error_log.clear();
   return ss.str();
 }
 
-void define_errors(jl::Module &mod) {
+void define_errors(jl::Module & m)
+{
   // Error checking and retrieval
-  mod.method("have_error", &have_error);
-  mod.method("get_and_clear_errors", &get_and_clear_errors);
-  mod.method("clear_error_log", &clear_error_log);
+  m.method("have_error", &have_error);
+  m.method("get_and_clear_errors", &get_and_clear_errors);
+  m.method("clear_error_log", &clear_error_log);
 
   // Helper function to safely execute a function and catch exceptions
   // This is used internally by other binding functions
-  mod.method("_try_catch_test", []() -> bool {
-    try {
+  m.method("_try_catch_test", []() -> bool {
+    try
+    {
       // Test that exception handling works
       throw std::runtime_error("test exception");
-    } catch (const std::runtime_error &e) {
+    } catch (const std::runtime_error & e)
+    {
       log_error(e.what());
       return false;
     }
@@ -49,4 +63,4 @@ void define_errors(jl::Module &mod) {
   });
 }
 
-} // namespace libsemigroups_julia
+}    // namespace libsemigroups_julia
